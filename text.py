5d8cf776e2b71e4a7d511b6e73dc1000c5e0142a
cadams2-arch <cadams2@ualr.edu>
2025-11-21 06:22:46 +0000

    done

--- text.py
+++ text.py
@@ -1,46 +1,46 @@
-#Trig Derivative Identities 
-#------------------------------
-# dx(sinx) = cosx
-# dx(cosx) = -sinx
-# dx(tanx) = sec^2x
-# dx(cscx) = -cotxcscx
-# dx(secx) = tanxsecx
-# dx(cotx) = -csc^2x
-#------------------------------
+print("Trig Derivative Identities")
+print("------------------------------")
+print("dx(sinx) = cosxprint")
+print("dx(cosx) = -sinx")
+print("dx(tanx) = sec^2x")
+print("dx(cscx) = -cotxcscx")
+print("dx(secx) = tanxsecx")
+print("dx(cotx) = -csc^2x")
+print("------------------------------")
 
-#Trig Integral Identities 
-#------------------------------
-#int(sinx)dx = -cosx + c
-#int(cosx)dx = sinx + c
-#int(csc^2x)dx = -cotx + c
-#int(sec^2x)dx = tanx + c
-#int(cscxcotx)dx = -cscx + c
-#int(secxtanx)dx = secx + c
-#------------------------------
+print("Trig Integral Identities")
+print("------------------------------")
+print("int(sinx)dx = -cosx + c")
+print("int(cosx)dx = sinx + c")
+print("int(csc^2x)dx = -cotx + c")
+print("int(sec^2x)dx = tanx + c")
+print("int(cscxcotx)dx = -cscx + c")
+print("int(secxtanx)dx = secx + c")
+print("------------------------------")
 
-#Natural Log Rules
-#------------------------------
-#ln(a•b) = lna + lnb
-#ln(a/b) = lna - lnb
-#ln(a^b) = blna
-#(lnx)' = 1/x
-#(lnu)' = (1/u)•u'
-#------------------------------
+print("Natural Log Rules")
+print("------------------------------")
+print("ln(a•b) = lna + lnb")
+print("ln(a/b) = lna - lnb")
+print("ln(a^b) = blna")
+print("(lnx)' = 1/x")
+print("(lnu)' = (1/u)•u'")
+print("------------------------------")
 
-#Inverse Function Rules
-#------------------------------
-#f(x) maps an x input to a y output
-#f^-1(x) maps a y input to an x output
-#replace f(x) with y, swap x and y, solve for inverse function 
-#one-to-one functions pass the horizonal line test
-#a horizontal line cannot touch 2 points at once on the HLT
-#even functions cannot be one-to-one
-#------------------------------
+print("Inverse Function Rules")
+print("------------------------------")
+print("f(x) maps an x input to a y output")
+print("f^-1(x) maps a y input to an x output")
+print("replace f(x) with y, swap x and y, solve for inverse function")
+print("one-to-one functions pass the horizonal line test")
+print("a horizontal line cannot touch 2 points at once on the HLT")
+print("even functions cannot be one-to-one")
+print("------------------------------")
 
-#Fundamental Theorem of Calculus
-#------------------------------
-#x=b/int/a means upper bound b, lower bound a 
-#let f(x) be continuous on [a,b]. Then, for a≤x≤b 
-#a) d/dx x/int/a(f(t))dt = f(x)
-#b) b/int/a (f(x))dx = F(b)-F(a) where F'(x)=f(x)
-#------------------------------
+print("Fundamental Theorem of Calculus")
+print("------------------------------")
+print("x=b/int/a means upper bound b, lower bound a")
+print("let f(x) be continuous on [a,b]. Then, for a≤x≤b")
+print("a) d/dx x/int/a(f(t))dt = f(x)")
+print("b) b/int/a (f(x))dx = F(b)-F(a) where F'(x)=f(x)")
+print("------------------------------")
