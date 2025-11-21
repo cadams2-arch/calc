0859e0d38c33dbe4dd14c065f24c1b513c285ace
cadams2-arch <cadams2@ualr.edu>
2025-11-21 06:03:59 +0000

    done

--- text.py
+++ text.py
@@ -1,1 +1,46 @@
+#Trig Derivative Identities 
+#------------------------------
+# dx(sinx) = cosx
+# dx(cosx) = -sinx
+# dx(tanx) = sec^2x
+# dx(cscx) = -cotxcscx
+# dx(secx) = tanxsecx
+# dx(cotx) = -csc^2x
+#------------------------------
 
+#Trig Integral Identities 
+#------------------------------
+#int(sinx)dx = -cosx + c
+#int(cosx)dx = sinx + c
+#int(csc^2x)dx = -cotx + c
+#int(sec^2x)dx = tanx + c
+#int(cscxcotx)dx = -cscx + c
+#int(secxtanx)dx = secx + c
+#------------------------------
+
+#Natural Log Rules
+#------------------------------
+#ln(a•b) = lna + lnb
+#ln(a/b) = lna - lnb
+#ln(a^b) = blna
+#(lnx)' = 1/x
+#(lnu)' = (1/u)•u'
+#------------------------------
+
+#Inverse Function Rules
+#------------------------------
+#f(x) maps an x input to a y output
+#f^-1(x) maps a y input to an x output
+#replace f(x) with y, swap x and y, solve for inverse function 
+#one-to-one functions pass the horizonal line test
+#a horizontal line cannot touch 2 points at once on the HLT
+#even functions cannot be one-to-one
+#------------------------------
+
+#Fundamental Theorem of Calculus
+#------------------------------
+#x=b/int/a means upper bound b, lower bound a 
+#let f(x) be continuous on [a,b]. Then, for a≤x≤b 
+#a) d/dx x/int/a(f(t))dt = f(x)
+#b) b/int/a (f(x))dx = F(b)-F(a) where F'(x)=f(x)
+#------------------------------
